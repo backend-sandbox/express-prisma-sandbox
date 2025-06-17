428fc7fcb3a9ab1841808669eb219d5585dbb9e0
Andriy Vovk <andriy.vovk@beetechy.com>
2025-06-17 20:21:47 +0300

    feat(service): add bulk upsert parks from csv file functionality in ParkService

--- src/services/park.service.ts
+++ src/services/park.service.ts
@@ -1,6 +1,10 @@
 // import { NotFound } from "http-errors";
 
+import errorConstant from "../constants/error.constant";
+import { CreateParkDto } from "../dtos/create-park.dto";
+import { mapRowToCreateParkDto } from "../mappers/row-to-park.mapper";
 import { ParkRepository } from "../repositories/park.repository";
+import { IncomingCsvPayload } from "../types/incoming-csv-payload.type";
 // import { PageResponse } from "../utils/page-response.util";
 // import { PageRequest } from "../utils/page-request.util";
 // import errorConstant from "../constants/error.constant";
@@ -38,5 +42,20 @@ export class ParkService {
   async upsertParkView(data: string) {
     // return await this.parkViewRepository.upsertParkView(data);
     return { success: true, data: data, message: "Park view updated successfully" };
+  }
+
+  // ! upsert parks from CSV file
+  async bulkUpsertParksFromCsv(parkObjects: IncomingCsvPayload) {
+    const { mapping, data } = parkObjects;
+
+    const parksToInsert: CreateParkDto[] = data.map((row) => {
+      try {
+        return mapRowToCreateParkDto(row, mapping);
+      } catch {
+        throw new Error(errorConstant.MAPPING_ROW_TO_PARK_FAILED);
+      }
+    });
+
+    return await this.parkRepository.bulkUpsertAndCountParks(parksToInsert);
   }
 }
